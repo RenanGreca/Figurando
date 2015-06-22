17c82e560921f890eb43326ff624daac33bafdb1
Renan Greca <renangreca@gmail.com>
2015-06-22 20:07:54 -0300

    coisas do renan

---
+++ Psico-Kids/Child.swift
@@ -0,0 +1,13 @@
+//
+//  Child.swift
+//  Psico-Kids
+//
+//  Created by Renan Greca on 6/22/15.
+//  Copyright (c) 2015 br.pucpr.bepid. All rights reserved.
+//
+
+import Cocoa
+
+class Child: NSObject {
+
+}
