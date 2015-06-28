21b30f8f16aa273209d481209bb8d3f47354b5ca
Matheus Cassarotti <matheus.cassarotti@gmail.com>
2015-06-28 12:10:42 -0300

    Tela de Login de educador

---
+++ Psico-Kids/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  Psico-Kids
+//
+//  Created by Matheus Cassarotti on 6/28/15.
+//  Copyright (c) 2015 br.pucpr.bepid. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+import LocalAuthentication
+
+class LoginViewController: UIViewController {
+
+
+
+
+
+}
