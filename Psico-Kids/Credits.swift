2a6fe844ef870d6d0de77d9a1b375fdfb3a44ba5
Samuel Lucas Ribeiro Cortez <SamCortez@MacBook-Pro-de-Samuel.local>
2015-06-29 00:34:41 -0300

    stuffing

--- Psico-Kids/Credits.swift
+++ Psico-Kids/Credits.swift
@@ -14,20 +14,9 @@ class Credits: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //self.navigationController?.navigationBar.titleTextAttributes = attributes
-        // Do any additional setup after loading the view, typically from a nib.
-        let image = UIImageView(frame: CGRectMake(10, 40, 250, 150))
-        image.image = UIImage(named: "bepid.png")
-        image.contentMode = UIViewContentMode.ScaleAspectFit
         
-        let text = UILabel(frame: CGRectMake(10, 10, 260, 20))
-        text.text = "App desenvolvido no BEPiD PUCPR"
-        text.textColor = UIColor.grayColor()
-        text.font = UIFont(name: "HelveticaNeue-Light", size: 16)
-        
-        
-        self.preferredContentSize.height = 320
-        self.preferredContentSize.width = 340
+        self.preferredContentSize.height = 370
+        self.preferredContentSize.width = 630
     }
     
     override func didReceiveMemoryWarning() {
